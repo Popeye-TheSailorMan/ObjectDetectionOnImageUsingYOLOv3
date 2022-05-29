6244c2b0e876fbc01b769b869e86f347ef4fbd3a
Abhisheksaxena <44522820+Abhisheksax@users.noreply.github.com>
2022-05-29 20:34:58 +0530

    "Added coco data"

--- yolo3image.py
+++ yolo3image.py
@@ -16,7 +16,7 @@ img = cv2.imread("images\woman-working-in-the-office.jpg")
 # cv2. waitKey(0)
 # cv2.destroyWindow("OriginalImage")
 
-print("Image Shape", img.shape)
+# print("Image Shape", img.shape)
 h, w = img.shape[:2] #getting height n width of the image using slicing
 
 #Using Blob on image (BLOB-> Binary Large OBject)
@@ -32,9 +32,24 @@ blob = cv2.dnn.blobFromImage(img,1/255.0,(416,416),swapRB=True,crop=False)
 blob_to_show = blob[0,:,:,:].transpose(1,2,0) #Shape : (416,416,3)
 
 # cv2.namedWindow("Blob Image", cv2.WINDOW_NORMAL)
-# cv2.imshow("Blob Image",blob_img)
+# cv2.imshow("Blob Image",cv2.cvtColor(blob_to_show,cv2.COLOR_RGB2BGR))
 # cv2.waitKey(0)
 # cv2.destroyWindow("Blob Image")
+
+#Importing YOLO
+#Data available for download
+# Available at: https://www.kaggle.com/datasets/valentynsichkar/yolo-coco-data
+
+with open('yolo-coco-data/coco.names') as f:
+    labels = [line.strip() for line in f]
+
+# print(labels)
+
+yolo = cv2.dnn.readNetFromDarknet('yolo-coco-data/yolov3.cfg',
+                                  'yolo-coco-data/yolov3.weights')
+all_layers = yolo.getLayerNames()
+
+
 
 
 
